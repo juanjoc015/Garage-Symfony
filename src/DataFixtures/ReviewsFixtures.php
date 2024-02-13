c5c89e2ca9046dbf74ba8dc3853636c38efc019c
Juan Jose <juanjo_c015@hotmail.com>
2024-02-13 18:12:49 +0100

    mise en place authentification

--- src/DataFixtures/ReviewsFixtures.php
+++ src/DataFixtures/ReviewsFixtures.php
@@ -6,18 +6,21 @@ use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use App\Entity\Review;
 
+
 class ReviewsFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
         // Aquí creas y cargas tus objetos Review
-        for ($i = 1; $i <= 3; $i++) {
+        for ($i = 1; $i <= 10; $i++) {
 
             $review = new Review();
             
             $review->setlastName('nom ' . $i)
                     ->setMessage('Contenu de la critique ' . $i)
-                    ->setRating(mt_rand(1, 5));
+                    ->setRating(mt_rand(1, 5))
+                    ->setCreatedAt(new \DateTime())
+                    ;
                 
 
             $manager->persist($review);
